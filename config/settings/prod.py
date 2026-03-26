46448f28318b1a04a6cd58f749ecd4eae462e568
AlessandroKuz <akuzcontact@gmail.com>
2026-03-26 15:07:59 +0100

    feat: improve security for production

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,20 +1,28 @@
-# config/settings/prod.py
-import os
+# import os
 
 from .base import *  # noqa: F401, F403
+from django.utils.csp import CSP
 
 DEBUG = False
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ.get("POSTGRES_HOST", "db"),
-        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
     }
 }
+
+
+# DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.postgresql",
+#        "NAME": os.environ["POSTGRES_DB"],
+#        "USER": os.environ["POSTGRES_USER"],
+#        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+#        "HOST": os.environ.get("POSTGRES_HOST", "db"),
+#        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+#    }
+# }
 
 STORAGES = {
     "staticfiles": {
@@ -27,12 +35,23 @@ COMPRESS_OFFLINE = (
     True  # pre-compiles everything at deploy time, no runtime compilation
 )
 
-# This is handled directly with Cloudflare
-# The alternative is to do it here
-SECURE_SSL_REDIRECT = False
-
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+SECURE_HSTS_SECONDS = 300
+# SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# behind reverse proxy / load balancer
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+SECURE_CSP = {
+    "default-src": [CSP.SELF],
+    "script-src": [CSP.SELF, CSP.NONCE],
+    "style-src": [CSP.SELF, CSP.NONCE, "https://fonts.googleapis.com"],
+    "font-src": [CSP.SELF, "https://fonts.gstatic.com"],
+}
